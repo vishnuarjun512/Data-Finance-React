51718d943b1bd5944575d7cfb82677363a3c064c
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-09-15 00:07:39 +0530

    Hero JSX Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,16 @@ const Hero = () => {
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
                 <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data</h1>
-                <div>
-                    <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, Flexible financing for </p>
-                    <Typed strings={['BTB','BTC','SASS']} typeSpeed={120} backSpeed={140} loop/>
+                <div className='flex justify-center items-center '>
+                    <p className='md:text-5xl sm:text-4xl text-2xl pl-2 font-bold inline-block'>Fast, Flexible financing for </p>
+                    <Typed strings={[' BTB',' BTC',' SASS']} typeSpeed={120} backSpeed={140} loop className='font-bold text-5xl pl-2'/>
                 </div>
 
+                <p className='md:text-2xl text-xl text-gray-500 pt-3 font-bold'>
+                    Monitor your data analytics to increase revenue for BNB, BTC & SAAS Platforms
+                </p>
+
+                <button className=' bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3'>Get Started</button>
             </div>
         </div>
     )
